982c2e8049748654d368a526e9a648d3be3473bd
Hisham Abdullah Bin Ateya <hishamco_2007@hotmail.com>
2015-07-02 09:58:45 +0300

    Add docs for 'ISessionStore' & 'DistributedSessionStore'

--- src/Microsoft.AspNet.Session/ISessionStore.cs
+++ src/Microsoft.AspNet.Session/ISessionStore.cs
@@ -6,10 +6,29 @@ using Microsoft.AspNet.Http.Features;
 
 namespace Microsoft.AspNet.Session
 {
+    /// <summary>
+    /// Represent a session store.".
+    /// </summary>
     public interface ISessionStore
     {
+        /// <summary>
+        /// Determine the session availability.".
+        /// </summary>
         bool IsAvailable { get; }
+        
+        /// <summary>
+        /// Connect to the session store.".
+        /// </summary>
         void Connect();
+        
+        /// <summary>
+        /// Create <see cref="ISession"/>.
+        /// </summary>
+        /// <param name="sessionId">Represents the session id.</param>
+        /// <param name="idleTimeout">Represents the session timeout.</param>
+        /// <param name="tryEstablishSession">Represents the delegate that trying to establish the session.</param>
+        /// <param name="isNewSessionKey">Determine whether the session key is new.</param>
+        /// <returns>The <see cref="ISession"/>.</returns>
         ISession Create(string sessionId, TimeSpan idleTimeout, Func<bool> tryEstablishSession, bool isNewSessionKey);
     }
 }
